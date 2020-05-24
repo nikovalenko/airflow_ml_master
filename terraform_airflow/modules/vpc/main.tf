a742c99dfbb6c8244a6df0e85c545b12c03cde56
nikovalenko <kovlnick7@gmail.com>
2020-05-24 03:39:17 +0300

    Fixing CB

--- terraform_airflow/modules/vpc/main.tf
+++ terraform_airflow/modules/vpc/main.tf
@@ -30,39 +30,39 @@ resource "aws_route_table" "route_table" {
     }
 }
 
-//resource "aws_route_table" "route_table_nat" {
-//    vpc_id     = "${aws_vpc.vpc.id}"
-//
-//    route {
-//        cidr_block = "0.0.0.0/0"
-//        gateway_id = "${aws_nat_gateway.gw.id}"
-//    }
-//
-//    tags {
-//        Name = "${var.name}-nat-route-table"
-//    }
-//}
+resource "aws_route_table" "route_table_nat" {
+    vpc_id     = "${aws_vpc.vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_nat_gateway.gw.id}"
+    }
+
+    tags {
+        Name = "${var.name}-nat-route-table"
+    }
+}
 
 resource "aws_route_table_association" "rt_association" {
   subnet_id      = "${var.subnet_id}"
   route_table_id = "${aws_route_table.route_table.id}"
 }
 
-//resource "aws_route_table_association" "cb_rt_association" {
-//  subnet_id = "${var.cb_subnet_id}"
-//  route_table_id = "${aws_route_table.route_table_nat.id}"
-//}
+resource "aws_route_table_association" "cb_rt_association" {
+  subnet_id = "${var.cb_subnet_id}"
+  route_table_id = "${aws_route_table.route_table_nat.id}"
+}
 
-//resource "aws_nat_gateway" "gw" {
-//  allocation_id = "${aws_eip.nat.id}"
-//  subnet_id     = "${var.subnet_id}"
-//  depends_on = ["aws_internet_gateway.gateway"]
-//
-//  tags = {
-//    Name = "NAT gateway"
-//  }
-//}
-//
-//resource "aws_eip" "nat" {
-//    vpc = true
-//}
+resource "aws_nat_gateway" "gw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = "${var.subnet_id}"
+  depends_on = ["aws_internet_gateway.gateway"]
+
+  tags = {
+    Name = "NAT gateway"
+  }
+}
+
+resource "aws_eip" "nat" {
+    vpc = true
+}
